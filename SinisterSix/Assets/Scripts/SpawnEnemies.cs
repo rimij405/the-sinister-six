2648a1a241dc1b997a1cd45cde66844f8de8c5fd
Ltd3086 <ltd3086@g.rit.edu>
2019-10-13 11:01:17 -0400

    Started spawn obj

---
+++ SinisterSix/Assets/Scripts/SpawnEnemies.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemies : MonoBehaviour
+{
+    public GameObject child;
+    public GameObject EnemyRef;
+    public float corruptionLevel;
+    public bool isCorrupted;
+    public bool isBeingDrained;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        child = Instantiate(EnemyRef, transform, true);
+        corruptionLevel = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (corruptionLevel == 0)
+        {
+            isCorrupted = false;
+        }
+    }
+}
