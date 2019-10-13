e19a6aaa0ff70d8eda4356c0b2a5e79e365eaaba
Lea Melendez <lrm4475@g.rit.edu>
2019-10-13 19:23:14 -0400

    UI
    UI fixes & animations

--- SinisterSix/Assets/Scripts/UI/UIMenu.cs
+++ SinisterSix/Assets/Scripts/UI/UIMenu.cs
@@ -5,12 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class UIMenu : MonoBehaviour
 {
+    //handles changing scenes when necessary
     public void ChangeScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
     }
+
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void ResetLevel()
+    {
+        SceneManager.LoadScene(Application.loadedLevel);
     }
 }
